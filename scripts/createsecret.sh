d87c12064caf780d841a94ab9201d55295f9e614
Siobhan Baynes <siba@microsoft.com>
2022-04-20 14:16:12 +0100

    Updated secret by script Wed Apr 20 14:16:11 BST 2022

--- scripts/createsecret.sh
+++ scripts/createsecret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for varName in CLIENTID CLIENTSECRET; do
+for varName in CLIENTID CLIENTSECRET DOCKER_REGISTRY_SERVER; do
     varVal=$(eval echo "\${$varName}")
     [[ -z $varVal ]] && {
         echo "💥 Error! Required variable '$varName' is not set!"
@@ -8,9 +8,10 @@ for varName in CLIENTID CLIENTSECRET; do
     }
 done
 
+export DOCKERCONFIGJSON=$(echo '{"auths":{"'$DOCKER_REGISTRY_SERVER'":{"username":"'$CLIENTID'","password":"'$CLIENTSECRET'","auth":"'$(echo -n $CLIENTID:$CLIENTSECRET | base64)'"}}}' | base64 --wrap=0)
 envsubst <./secret.template.yaml >../kustomize/secret.yaml
 sops --encrypt --in-place ../kustomize/secret.yaml
 
-git add ../kustomize/secret.yaml
-git commit -m "Updated secret by script $(date)"
-git push
+# git add ../kustomize/secret.yaml
+# git commit -m "Updated secret by script $(date)"
+# git push
